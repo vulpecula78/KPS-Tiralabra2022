90369afc53dd58c2f12604a5aee829dca56e4fab
Tino Pönni <tino.ponni@tutanota.com>
2022-03-07 12:59:23 +0200

    Koodia siistitty ja dokumentoitu.

--- src/ui/gorn_ui.py
+++ src/ui/gorn_ui.py
@@ -3,6 +3,9 @@ from termcolor import colored, cprint
 
 class GornUi:
     def __init__(self):
+        '''Alustaa käyttöliittymän tulosteet, sekä asettaa pelimoodiksi oletuksena
+        kivi, paperi ja sakset.
+        '''
         os.system('color') #Tarvitaan, jotta termcolor toimii windowsissa
         self.items = {"k":"kiven", "p":"paperin", "s":"sakset", "l":"liskon", "v":"Spockin"}
         self.results = {-1:"Pelaajan voitto", 0:"Tasapeli", 1:"Tietokoneen voitto"}
@@ -24,6 +27,13 @@ class GornUi:
                 'green'))
 
     def start_menu(self):
+        '''Pelin Päävalikko jota kutsutaan pääohjelmasta.
+        Pelaaja voi valita vastustajan, pelimoodin tai tilastot.
+
+        returns:
+            selection: Pelaajan valinta.
+            self.gorn: true tai false: onko pelimoodi kps vai kpslv
+        '''
         menu_items = ['1', '2', '3', '4', '5', '6', '0']
 
         while True:
@@ -49,6 +59,19 @@ class GornUi:
                 return selection, self.gorn
 
     def game_menu(self, round_, won, ai_won, ties):
+        '''Pelinaikainen valikko. gorn.py kutsuma funktio, joka kertoo sen hetkisen
+           kierroksen ja pelitilanteen, sekä kertoo pelaajalle mahdolliset vaihtoehdot.
+           Funktio on loopissa, kunnes pelaaja syöttää sallitun vaihtoehdon.
+
+        Args:
+            round_: pelattu kierrosmäärä.
+            won: pelaajan voittamat kierrokset.
+            ai_won: AI:n voittamat kierrokset.
+            ties: tasapelit.
+
+        returns:
+            choice: pelaajan tekemä valinta.
+        '''
         if self.gorn:
             choices = ('x', 'k', 's', 'p', 'l', 'v')
         else:
@@ -72,6 +95,7 @@ class GornUi:
             cprint('Virheellinen valinta!', 'red')
 
     def move(self, player, computer, result):
+        '''Tulostaa valitut siirrot ja lopputuloksen.'''
         print("Valitsit " + self.items[player] + " ja tietokone valitsi " + self.items[computer])
         if result == 1:
             cprint(self._sentences[computer+player], 'red')
@@ -92,16 +116,16 @@ class GornUi:
                f" ({data[4]:.2f}%) ja tietokone {data[2]} ({data[5]:.2f}%)." +
                f" Tasapelejä pelattu: {data[3]} ({data[6]:.2f}%)\n", 'green')
         if len(stats_by_rounds) > 0:
-            for i in range(len(stats_by_rounds)):
-                print(f"Kierrokset {i*25+1} - {(i+1)*25}: Pelaaja: {stats_by_rounds[i][0]:.2f}%," +
-                      f" AI: {stats_by_rounds[i][1]:.2f}%," +
-                      f" Tasapelit: {stats_by_rounds[i][2]:.2f}%")
+            for i, stats in enumerate(stats_by_rounds): #in range(len(stats_by_rounds)):
+                print(f"Kierrokset {i*25+1} - {(i+1)*25}: Pelaaja: {stats[0]:.2f}%," +
+                      f" AI: {stats[1]:.2f}%," +
+                      f" Tasapelit: {stats[2]:.2f}%")
             print()
-        if type(probabilities[0]) == float:
+        if isinstance(probabilities[0], float):
             print("Todennäköisyystaulukko, jonka mukaan pelaajan seuraava siirto ennakoidaan:")
             items = ['k', 'p', 's', 'l', 'v']
-            for i in range(len(probabilities)):
-                print(f"{items[i]}: {probabilities[i]:.2f}% ", end="")
+            for i, prob in enumerate(probabilities):
+                print(f"{items[i]}: {prob * 100:.2f}% ", end="")
         else:
             print("Käytössä oleva mallinnus:")
             for i in probabilities:
